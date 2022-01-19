9997e947bc89151b723dcded301ee94570b1086a
Manoj Kumar Ragupathi <manojkumar.ragupathi@outlook.com>
2022-01-19 22:03:59 +0530

    video detection working

--- detectvideo.py
+++ detectvideo.py
@@ -7,35 +7,138 @@ if len(physical_devices) > 0:
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
-from core.yolov4 import filter_boxes
+from core.yolov4 import filter_boxes, decode, YOLO
 from tensorflow.python.saved_model import tag_constants
 from PIL import Image
 import cv2
 import numpy as np
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
+import os, re, os.path
+
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
 
 flags.DEFINE_string("framework", "tf", "(tf, tflite, trt")
-flags.DEFINE_string("weights", "./checkpoints/yolov4-416", "path to weights file")
+flags.DEFINE_string(
+    "weights", "./checkpoints/yolov4-416/variables/variables", "path to weights file"
+)
 flags.DEFINE_integer("size", 416, "resize images to")
 flags.DEFINE_boolean("tiny", False, "yolo or yolo-tiny")
 flags.DEFINE_string("model", "yolov4", "yolov3 or yolov4")
 flags.DEFINE_string("video", "./data/road.mp4", "path to input video")
 flags.DEFINE_float("iou", 0.45, "iou threshold")
 flags.DEFINE_float("score", 0.25, "score threshold")
-flags.DEFINE_string("output", None, "path to output video")
+flags.DEFINE_string("output", "./data/roadnew.mp4", "path to output video")
 flags.DEFINE_string(
-    "output_format", "XVID", "codec used in VideoWriter when saving video to file"
+    "output_format", "mp4v", "codec used in VideoWriter when saving video to file"
 )
 flags.DEFINE_boolean(
     "dis_cv2_window", False, "disable cv2 window during the process"
 )  # this is good for the .ipynb
 
 
+# @tf.function
+def infer(batch_data, model):
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    # batch_data = tf.constant(image_data)
+    feature_maps = model(batch_data)
+    bbox_tensors = []
+    prob_tensors = []
+    if FLAGS.tiny:
+        for i, fm in enumerate(feature_maps):
+            if i == 0:
+                output_tensors = decode(
+                    fm,
+                    FLAGS.size // 16,
+                    NUM_CLASS,
+                    STRIDES,
+                    ANCHORS,
+                    i,
+                    XYSCALE,
+                    FLAGS.framework,
+                )
+            else:
+                output_tensors = decode(
+                    fm,
+                    FLAGS.size // 32,
+                    NUM_CLASS,
+                    STRIDES,
+                    ANCHORS,
+                    i,
+                    XYSCALE,
+                    FLAGS.framework,
+                )
+            bbox_tensors.append(output_tensors[0])
+            prob_tensors.append(output_tensors[1])
+    else:
+        for i, fm in enumerate(feature_maps):
+            if i == 0:
+                output_tensors = decode(
+                    fm,
+                    FLAGS.size // 8,
+                    NUM_CLASS,
+                    STRIDES,
+                    ANCHORS,
+                    i,
+                    XYSCALE,
+                    FLAGS.framework,
+                )
+            elif i == 1:
+                output_tensors = decode(
+                    fm,
+                    FLAGS.size // 16,
+                    NUM_CLASS,
+                    STRIDES,
+                    ANCHORS,
+                    i,
+                    XYSCALE,
+                    FLAGS.framework,
+                )
+            else:
+                output_tensors = decode(
+                    fm,
+                    FLAGS.size // 32,
+                    NUM_CLASS,
+                    STRIDES,
+                    ANCHORS,
+                    i,
+                    XYSCALE,
+                    FLAGS.framework,
+                )
+            bbox_tensors.append(output_tensors[0])
+            prob_tensors.append(output_tensors[1])
+
+    pred_bbox = tf.concat(bbox_tensors, axis=1)
+    pred_prob = tf.concat(prob_tensors, axis=1)
+    if FLAGS.framework == "tflite":
+        pred_bbox = (pred_bbox, pred_prob)
+    else:
+        boxes, pred_conf = filter_boxes(
+            pred_bbox,
+            pred_prob,
+            score_threshold=FLAGS.score,
+            input_shape=tf.constant([FLAGS.size, FLAGS.size]),
+        )
+        pred_bbox = tf.concat([boxes, pred_conf], axis=-1)
+    boxes = pred_bbox[:, :, 0:4]
+    pred_conf = pred_bbox[:, :, 4:]
+
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])
+        ),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=FLAGS.iou,
+        score_threshold=FLAGS.score,
+    )
+    return boxes, scores, classes, valid_detections
+
+
 def main(_argv):
-    config = ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = InteractiveSession(config=config)
+    # config = ConfigProto()
+    # config.gpu_options.allow_growth = True
+    # session = InteractiveSession(config=config)
     STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
     input_size = FLAGS.size
     video_path = FLAGS.video
@@ -51,10 +154,13 @@ def main(_argv):
         print(input_details)
         print(output_details)
     else:
-        saved_model_loaded = tf.saved_model.load(
-            FLAGS.weights, tags=[tag_constants.SERVING]
-        )
-        infer = saved_model_loaded.signatures["serving_default"]
+        # saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+        # infer = saved_model_loaded.signatures[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
+        STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+        inputs = tf.keras.layers.Input([FLAGS.size, FLAGS.size, 3])
+        outputs = YOLO(inputs, NUM_CLASS, FLAGS.model, FLAGS.tiny)
+        model = tf.keras.Model(inputs, outputs)
+        model.load_weights(FLAGS.weights)
 
     if FLAGS.output:
         # by default VideoCapture returns float instead of int
@@ -65,6 +171,12 @@ def main(_argv):
         out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
 
     frame_id = 0
+    ### Code to remove output images generated in previous run
+    mypath = "./output/"
+    for root, dirs, files in os.walk(mypath):
+        for file in files:
+            os.remove(os.path.join(root, file))
+    #############################################################            
     while True:
         return_value, frame = vid.read()
         if return_value:
@@ -105,32 +217,14 @@ def main(_argv):
                 )
         else:
             batch_data = tf.constant(image_data)
-            pred_bbox = infer(batch_data)
-            for key, value in pred_bbox.items():
-                boxes = value[:, :, 0:4]
-                pred_conf = value[:, :, 4:]
-
-        (
-            boxes,
-            scores,
-            classes,
-            valid_detections,
-        ) = tf.image.combined_non_max_suppression(
-            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-            scores=tf.reshape(
-                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])
-            ),
-            max_output_size_per_class=50,
-            max_total_size=50,
-            iou_threshold=FLAGS.iou,
-            score_threshold=FLAGS.score,
-        )
+            boxes, scores, classes, valid_detections = infer(batch_data, model)
         pred_bbox = [
             boxes.numpy(),
             scores.numpy(),
             classes.numpy(),
             valid_detections.numpy(),
         ]
+        print(pred_bbox)
         image = utils.draw_bbox(frame, pred_bbox)
         curr_time = time.time()
         exec_time = curr_time - prev_time
@@ -139,9 +233,11 @@ def main(_argv):
         print(info)
 
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(f"./output/{time.time()}.jpg", result)
         if not FLAGS.dis_cv2_window:
-            cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
-            cv2.imshow("result", result)
+            # cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
+
+            # cv2.imshow("result", result)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
